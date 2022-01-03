de716a95219266a0e2a3e06493845c1a9efb690e
verbit7sky <87092911+verbit7sky@users.noreply.github.com>
2022-01-03 10:40:08 +0000

    Deploying to gh-pages from @ verbit7sky/goit-js-hw-08@0ab7d826abf4d6af9d378caf28c3a2503298aeae 🚀

--- 02-video.02221a71.js
+++ 02-video.02221a71.js
@@ -33,6 +33,6 @@ var i=require("./isObject"),t=require("./now"),r=require("./toNumber"),n="Expect
 },{"./isObject":"u9vI","./now":"pJf5","./toNumber":"iS0Z"}],"Ywd4":[function(require,module,exports) {
 var i=require("./debounce"),e=require("./isObject"),n="Expected a function";function r(r,t,a){var o=!0,u=!0;if("function"!=typeof r)throw new TypeError(n);return e(a)&&(o="leading"in a?!!a.leading:o,u="trailing"in a?!!a.trailing:u),i(r,t,{leading:o,maxWait:t,trailing:u})}module.exports=r;
 },{"./debounce":"CXfR","./isObject":"u9vI"}],"iiAF":[function(require,module,exports) {
-"use strict";var e=r(require("@vimeo/player")),t=r(require("lodash/throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const n="videoplayer-current-time",o=document.querySelector("iframe"),u=new e.default(o);function i(){u.getCurrentTime().then(function(e){localStorage.setItem(n,JSON.stringify(e))})}u.on("timeupdate",(0,t.default)(i,1e3));const a=JSON.parse(localStorage.getItem(n));u.setCurrentTime(a);
+"use strict";var e=r(require("@vimeo/player")),t=r(require("lodash/throttle"));function r(e){return e&&e.__esModule?e:{default:e}}const n="videoplayer-current-time",o=document.querySelector("iframe"),u=new e.default(o);function i(){u.getCurrentTime().then(function(e){localStorage.setItem(n,JSON.stringify(e))})}u.on("timeupdate",(0,t.default)(i,1e3));const a=JSON.parse(localStorage.getItem(n)||0);u.setCurrentTime(a);
 },{"@vimeo/player":"zU8P","lodash/throttle":"Ywd4"}]},{},["iiAF"], null)
-//# sourceMappingURL=/goit-js-hw-08/02-video.2d8e4a2e.js.map
+//# sourceMappingURL=/goit-js-hw-08/02-video.02221a71.js.map
